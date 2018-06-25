7d463e1ef9acb15457344afc2200735983fdb1e7
example <example@example.com>
2018-06-25 19:00:15 +0800

    解决图片图层被缩放问题，并使用白色背景来补偿宽高

--- java-version/com/scan/CommandLine.java
+++ java-version/com/scan/CommandLine.java
@@ -9,6 +9,16 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
+ * 用途：
+ * <p>
+ * 识别所有图片（不包含"cover"目录），找出最大"宽、高"，然后应用到所有图片
+ * <p>
+ * "PATH"目录中子目录"cover"放封面，不处理为黑白，但与黑白保持相同的宽高（IS_BLACK_WHITE控制正文是否转成黑白：true或false）
+ * <p>
+ * "OUT_DIR"目录为输出目录，包含了彩色封面和黑白正文
+ * <p>
+ * 步骤：
+ * <p>
  * 1. 扫描图片
  * <p>
  * 2. ABBYY - 编辑图像
@@ -17,7 +27,7 @@ import java.util.concurrent.ThreadPoolExecutor;
  * <p>
  * 3. 文件 - 将页面保存为图像 - (保存类型: jpeg 彩色)
  * <p>
- * 4. 执行 CommandLine 去除exif并统一宽度
+ * 4. 执行 CommandLine 去除exif并统一宽度等（见：用途）
  * <p>
  * 5. Adobe Acrobat Pro DC - 创建PDF
  * 5.1 工具 - 合并文件 - 添加文件 - 选项(文件大小: 默认大小, 其它选项: 取消所有勾选) - 合并
@@ -28,10 +38,10 @@ public class CommandLine {
     private final static String PATH = "/Users/osx/Desktop/ttt/新建文件夹";// 处理目录【修改】
     private final static String SUFFIX = "jpg";// "处理目录"中的指定图片后缀【修改】
     private final static String CONVERT = "/usr/local/opt/imagemagick@6/bin/convert";// imagemagick路径【修改】
-    private final static long WIDTH = 2048; // 输出宽度（像素）【修改】
     private final static int MAX_THREAD = 5;// 最大线程数【修改】
     private final static File OUT_DIR = new File(PATH, "_resize");// 输出目录
     private final static boolean IS_BLACK_WHITE = true;// 是否转成黑白图像【修改】
+    // 不需要高度是因为转黑白之前已经统一了宽高
     private final static int BLACK_WHITE_WIDTH = 3072;// 黑白图像宽度（像素）【修改】
     // 不转黑白图像只更改大小的封面图片路径，在"处理目录"下，会生成在"输出目录中"【修改】
     private final static String BLACK_WHITE_COVER = new File(PATH, "cover").getAbsolutePath();
@@ -51,8 +61,26 @@ public class CommandLine {
     }
 
     public static void main(String[] args) {
+        // 读取文件
         List<File> fileList = getFileWithSuffixDepth1(PATH, SUFFIX);
-
+        // 找到最大：宽、高
+        int maxWidth = 0;
+        int maxHeight = 0;
+        for (final File file : fileList) {
+            try {
+                BufferedImage image = ImageIO.read(file);
+                if (image.getWidth() > maxWidth)
+                    maxWidth = image.getWidth();
+                if (image.getHeight() > maxHeight)
+                    maxHeight = image.getHeight();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        final int fMaxWidth = maxWidth;
+        final int fMaxHeight = maxHeight;
+        System.out.println("最大宽高：" + maxWidth + "x" + maxHeight);
+        // 处理图片
         final ThreadPoolExecutor fixedThreadPool = (ThreadPoolExecutor)
                 Executors.newFixedThreadPool(MAX_THREAD);
         int i = 0;
@@ -73,29 +101,31 @@ public class CommandLine {
                         StringBuffer commandLine = new StringBuffer();
                         // 命令可自定义
                         // ----- jpg
-                        if (!IS_BLACK_WHITE)
-                            commandLine.append(CONVERT).append(" ")
-                                    .append("-resize").append(" ").append(WIDTH).append(" ")
-                                    .append("-gravity").append(" ").append("center").append(" ")
-                                    .append("-extent").append(" ").append(WIDTH).append(" ")
-                                    .append(outFile.getAbsolutePath()).append(" ")
-                                    .append(outFile.getAbsolutePath()).append(" ")
-                                    .append(" ");
+                        commandLine.append(CONVERT).append(" ")
+                                .append("-gravity").append(" ").append("center").append(" ")
+                                // 仅扩大背景，不缩放原始图像大小
+                                .append("-extent").append(" ").append(fMaxWidth).append("x").append(fMaxHeight).append(" ")
+                                .append(outFile.getAbsolutePath()).append(" ")
+                                .append(outFile.getAbsolutePath()).append(" ")
+                                .append(" ");
+//                        System.out.println(commandLine);
+                        exec(commandLine.toString());
+                        commandLine.delete(0, commandLine.length());
                         // ----- Black & White png
-                        if (IS_BLACK_WHITE)
+                        if (IS_BLACK_WHITE) {
                             commandLine.append(CONVERT).append(" ")
-                                    .append("-resize").append(" ").append(BLACK_WHITE_WIDTH).append(" ")
                                     .append("-gravity").append(" ").append("center").append(" ")
-                                    .append("-extent").append(" ").append(BLACK_WHITE_WIDTH).append(" ")
+                                    // 缩放整张图片宽度（高度会自动处理）
+                                    .append("-resize").append(" ").append(BLACK_WHITE_WIDTH).append(" ")
                                     .append("-monochrome").append(" ")
                                     .append(outFile.getAbsolutePath()).append(" ")
                                     .append(getPrefix(outFile.getAbsolutePath())).append(".png").append(" ")
                                     .append(" ");
 
 //                        System.out.println(commandLine);
-                        exec(commandLine.toString());
-                        if (IS_BLACK_WHITE)
+                            exec(commandLine.toString());
                             deleteFile(outFile);// 生成黑白png后删除jpg
+                        }
                     } catch (Exception e) {
                         System.out.println("exception:" + file.getName());
                         e.printStackTrace();
@@ -137,14 +167,26 @@ public class CommandLine {
                     // 命令可自定义
                     // ----- jpg
                     commandLine.append(CONVERT).append(" ")
-                            .append("-resize").append(" ").append(BLACK_WHITE_WIDTH).append(" ")
                             .append("-gravity").append(" ").append("center").append(" ")
-                            .append("-extent").append(" ").append(BLACK_WHITE_WIDTH).append(" ")
+                            // 仅扩大背景，不缩放原始图像大小
+                            .append("-extent").append(" ").append(fMaxWidth).append("x").append(fMaxHeight).append(" ")
                             .append(outFile.getAbsolutePath()).append(" ")
                             .append(outFile.getAbsolutePath()).append(" ")
                             .append(" ");
+//                        System.out.println(commandLine);
                     exec(commandLine.toString());
+                    commandLine.delete(0, commandLine.length());
+                    // ----- 缩放
+                    commandLine.append(CONVERT).append(" ")
+                            .append("-gravity").append(" ").append("center").append(" ")
+                            // 缩放整张图片宽度（高度会自动处理）
+                            .append("-resize").append(" ").append(BLACK_WHITE_WIDTH).append(" ")
+                            .append(outFile.getAbsolutePath()).append(" ")
+                            .append(outFile.getAbsolutePath()).append(" ")
+                            .append(" ");
 
+//                        System.out.println(commandLine);
+                    exec(commandLine.toString());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
